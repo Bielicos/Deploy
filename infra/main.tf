3aa71f0fcd526b2763cc9aab6abc98cc9fb7bfe7
bielico <gkortschinski@gmail.com>
2025-08-16 10:49:39 -0300

    feat: Route-table adicionado e sua associação com o VPC foi configurada

--- infra/main.tf
+++ infra/main.tf
@@ -39,11 +39,6 @@ resource "aws_security_group" "securitygroup" {
   }
 }
 
-resource "aws_key_pair" "minha_keypair" {
-  key_name = "minha_keypair"
-  public_key = file("~/.ssh/id_ed25519.pub")
-}
-
 resource "aws_vpc" "minha_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
@@ -68,10 +63,35 @@ resource "aws_vpc" "minha_vpc" {
     }
   }
 
-resource "aws_instance" "meu_ec3" {
-  ami           = "ami-0de716d6197524dd9"
-  instance_type = "t3.micro"
-  user_data = file("user_data.sh")
-  key_name = aws_key_pair.minha_keypair
-  vpc_security_group_ids = [aws_security_group.securitygroup.id]
-}
+  resource "aws_route_table" "meu_rt" {
+    vpc_id = aws_vpc.minha_vpc.id
+
+    route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.meu_ig.id
+    }
+
+    tags = {
+      Name = "meu_rt"
+    }
+  }
+
+  resource "aws_route_table_association" "meu_rta" {
+    route_table_id = aws_route_table.meu_rt.id
+    subnet_id = aws_subnet.minha_subnet.id
+  }
+
+    resource "aws_key_pair" "minha_keypair" {
+      key_name = "minha_keypair"
+      public_key = file("~/.ssh/id_ed25519.pub")
+    }
+
+    resource "aws_instance" "meu_ec2" {
+          ami           = "ami-0de716d6197524dd9"
+          instance_type = "t3.micro"
+          subnet_id = aws_subnet.minha_subnet.id
+          associate_public_ip_address = true
+          key_name = aws_key_pair.minha_keypair
+          vpc_security_group_ids = [aws_security_group.securitygroup.id]
+          user_data = file("user_data.sh")
+    }
